1ef75a542f26b40ff7cf6af9194eb1e44aaa6395
RiccardoCantoni <ricc.cantoni@gmail.com>
2018-05-27 15:29:06 +0200

    stage cleaning

--- ArchitectureGeneration/Assets/uberMain.cs
+++ ArchitectureGeneration/Assets/uberMain.cs
@@ -7,19 +7,24 @@ public class uberMain : MonoBehaviour {
 
 	public float loopTime;
 	public bool takeScreenshots;
+    int loops;
 
 	void Start () {
+        loops = 0;
 		StartCoroutine (megaLoop ());
 	}
 
 	IEnumerator megaLoop(){
+        
 		yield return new WaitForSeconds (loopTime);
 		if (takeScreenshots) {
 			string s1 = Random.value.ToString ();
 			string s2 = Random.value.ToString ();
 			string s3 = Random.value.ToString ();
-			ScreenCapture.CaptureScreenshot ("screenshots/"+s1 + s2 + s3 + ".png");
+			ScreenCapture.CaptureScreenshot ("screenshots/"+loops+"-"+s1 + s2 + s3 + ".png",2);
+            loops++;
 		}
-		SceneManager.LoadSceneAsync (0);
+        if (loops<50)
+		    SceneManager.LoadSceneAsync (0);
 	}
 }
