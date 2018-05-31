5a0349613568ebb67cd4e466779ac5e796302293
RiccardoCantoni <ricc.cantoni@gmail.com>
2018-05-31 23:33:13 +0200

    plain building prefabl pulling refactored

--- ArchitectureGeneration/Assets/Resources/data/building data/PlainBuildingData.cs
+++ ArchitectureGeneration/Assets/Resources/data/building data/PlainBuildingData.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class PlainBuildingData : MonoBehaviour {
 
-	public List<GameObject> windowPrefabs;
-
-	public PlainBuildingDataStruct getDataStruct(BuildingFoundation foundation){
+		public PlainBuildingDataStruct getDataStruct(BuildingFoundation foundation){
 		PlainBuildingDataStruct data = new PlainBuildingDataStruct ();
 		data.sizeX = foundation.lengthX;
 		data.sizeZ = foundation.lengthZ;
 		data.floorCount = Randomiser.intBetween (2, 3);
-		data.windowPrefab = windowPrefabs [Random.Range (0, windowPrefabs.Count)];
+        GameObject[] windowPrefabs = GenericUtils.loadAllPrefabs("prefabs/generic/windows");
+        data.windowPrefab = windowPrefabs [Random.Range (0, windowPrefabs.Length)];
         data.wallPrefab = GenericUtils.loadPrefab("plain building", "wallPrefab");
 		data.fullFloorPrefab = GenericUtils.loadPrefab("plain building", "fullFloorPrefab");
         data.roofPrefab = GenericUtils.loadPrefab("plain building", "roofPrefab");
